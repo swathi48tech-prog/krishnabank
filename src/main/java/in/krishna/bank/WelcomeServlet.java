28a222a43cdaadf19514a99feb8bebb9c0fcbdd8
krishna <kmcrazybng@gmail.com>
2025-12-02 03:47:34 +0000

    deleted out

--- src/main/java/in/krishna/bank/WelcomeServlet.java
+++ src/main/java/in/krishna/bank/WelcomeServlet.java
@@ -21,10 +21,7 @@ public class WelcomeServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
-        PrintWriter out = response.getWriter();
-        PrintWriter out = response.getWriter();
-        PrintWriter out = response.getWriter();
-        PrintWriter out = response.getWriter();
+
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("<head>");
